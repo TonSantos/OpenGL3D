0a12709068f3788608e13bfdd665026cef2666db
Glairton Santos <glairtons@gmail.com>
2018-08-31 00:23:06 -0300

    funcoes para desenhar formas geometricas

--- OpenGL3D/Main.cpp
+++ OpenGL3D/Main.cpp
@@ -4,24 +4,68 @@
 #include <iostream>
 
 #include "Vector3.h"
+#include "Util.h"
 
-void draw()
+void drawVertices(Vector3 vertices[], int size)
 {
-	/*lista de vertices*/
-	Vector3 vertices[2] = { 
-			Vector3(0.0, 0.0, 0.0),  Vector3(1.0, 1.0, 0.0)
+	for (int i = 0; i < size; ++i)
+	{
+		glVertex3f(vertices[i].getCoordX(), vertices[i].getCoordY(), vertices[i].getCoordZ());
+	}
+}
+
+void drawRectangle()
+{
+	/*RETANGULO - lista de vertices*/
+	Vector3 verticesRetangulo[4] = {
+			Vector3(2.0, 4.0, 0.0), Vector3(8.0, 4.0, 0.0),
+			Vector3(8.0, 6.0, 0.0), Vector3(2.0, 6.0, 0.0)
 	};
 
 	glLineWidth(5);
-	glColor3f(0, 1, 0);
 
+	glColor3f(1, 0, 0);
+	glBegin(GL_POLYGON);
+
+	drawVertices(verticesRetangulo, 4);
+}
+
+void drawLine()
+{
+	/*LINHA - lista de vertices*/
+	Vector3 verticesLinha[2] = {
+			Vector3(0.0, 0.0, 0.0),  Vector3(3.0, 3.0, 0.0)
+	};
+
+	glColor3f(0, 1, 0);
 	glBegin(GL_LINES);
 
-	glVertex3f(vertices[0].getCoordX(), vertices[0].getCoordY(), vertices[0].getCoordZ());
-	glVertex3f(vertices[1].getCoordX(), vertices[1].getCoordY(), vertices[1].getCoordZ());
+	drawVertices(verticesLinha, 2);
 
-	/*glVertex3f(3, 3, 0);
-	glVertex3f(-3, -3, 0);*/
+}
+
+void drawTriangle()
+{
+
+	/*TRIANGULO - lista de vertices*/
+	Vector3 verticesTriangulo[3] = {
+			Vector3(1.0, 1.0, 1.0),  Vector3(5.0, 1.0, 1.0), Vector3(1.0, 5.0, 1.0)
+	};
+
+	glColor3f(0, 1, 1);
+	glBegin(GL_TRIANGLES);
+
+	drawVertices(verticesTriangulo, 3);
+
+}
+
+void draw()
+{
+
+	//drawLine();
+	//drawRectangle();
+	drawTriangle();
+
 	glEnd();
 
 	glutSwapBuffers();
@@ -32,6 +76,12 @@ int main(int argc, char ** argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutCreateWindow("OPENGL 3D");
+
+	glMatrixMode(GL_PROJECTION);              // setup viewing projection 
+	glLoadIdentity();                           // start with identity matrix 
+
+	glOrtho(0.0, 10.0, 0.0, 10.0, -5.0, 5.0);   // setup a 10x10x20 viewing world
+
 	glutDisplayFunc(draw);
 
 	GLenum err = glewInit();
